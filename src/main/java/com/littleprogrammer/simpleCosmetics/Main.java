fced76f52b2bf5302a1ddda9ad84829f422b4e33
Garrett <littleprogrammercox@gmail.com>
2023-10-14 21:50:21 -0600

    First commit.

---
+++ src/main/java/com/littleprogrammer/simpleCosmetics/Main.java
@@ -0,0 +1,12 @@
+package com.littleprogrammer.simpleCosmetics;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main {
+
+    public static final String MODID = "simpleCosmetics";
+    public static final String VERSION = "1.0";
+
+
+}
